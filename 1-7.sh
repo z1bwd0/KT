8eb5dd660c7895905267aeeef0e8eb482f8d91e4
z1bwd0 <oleg.aizvert@mail.ru>
2021-12-01 00:14:41 +0300

    first commit

---
+++ 1-7.sh
@@ -0,0 +1,43 @@
+# 1 zadanie
+hostname
+# 2 zadanie
+which gcc
+gcc --version
+# 3 zadanie 
+glibc - ldd --version
+# 4 zadanie
+free
+# 5 zadanie
+df -h
+findmnt --real / blkid
+# 6 Zadanie
+ lsblk
+# 7 zadanie
+./hi
+
+# 8 zadanie
+python hi.py
+
+
+# 12 zadanie
+ compgen -u
+
+#13 zadanie
+ps aux
+
+#14 zadanie
+ifconfig -a
+
+#15 zadanie
+lsusb -v
+
+#16 zadanie
+lshw
+
+# 17 zadanie
+lscpu
+
+# 18 zadanie
+
+# 19 zadanie 
+dpkg -S *.ko | grep /lib/modules
